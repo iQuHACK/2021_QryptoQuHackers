947aa0f49bd75fcc3a92cc370260a331c5f169f1
Steven Raphael <raphael4@mit.edu>
2021-01-30 18:38:09 +0000

    added functionality for bigger hashes

--- swap_test.py
+++ swap_test.py
@@ -7,26 +7,28 @@ provider = IonQProvider(token='BFmvdArkiCbsS12r4LZf5VgYDo4HngsS')
 
 
 
-q = QuantumRegister(3)
-c = ClassicalRegister(1)
 
 
-swaptest = QuantumCircuit(q,c)
+def swaptest(size):
+    q = QuantumRegister(2*size+1)
+    c = ClassicalRegister(1)    
+    swaptest = QuantumCircuit(q,c)
+    swaptest.h(0)
+    #swaptest.append(hashfunc, [2])
+    for i in range(1, size+1):
+        swaptest.cx(i+size,i)
+        swaptest.toffoli(0,i,i+size)
+        swaptest.cx(i+size,i)
+    swaptest.h(0)
+    swaptest.measure(range(1), range(1))
+    return swaptest
 
-swaptest.h(0)
-#swaptest.append(hashfunc, [2])
-swaptest.cx(2,1)
-swaptest.toffoli(0,1,2)
-swaptest.cx(2,1)
-swaptest.h(0)
-
-swaptest.measure(range(1), range(1))
-
-print(swaptest.draw())
-backend = provider.get_backend("ionq_qpu")
+swap3=swaptest(3)
+print(swap3.draw())
+backend = provider.get_backend("ionq_simulator")
 #backend_sim = Aer.get_backend('qasm_simulator')
 #job_sim = execute(circ, backend_sim, shots=1024)
-job_sim = backend.run(swaptest, shots=1024)
+job_sim = backend.run(swap3, shots=1024)
 result_sim = job_sim.result()
 
 counts = result_sim.get_counts()
